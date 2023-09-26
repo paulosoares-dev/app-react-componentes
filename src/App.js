82eba5546123840de9494d999f34fed4c8daa617
Paulo Soares <contato@ptek.com.br>
2023-09-26 17:45:46 -0300

    Incrementado novos componentes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
